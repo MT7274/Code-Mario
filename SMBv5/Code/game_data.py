9dbd854258d9623e883b161b1650eb1e19dea258
MT7274 <131871961+MT7274@users.noreply.github.com>
2023-04-27 06:52:44 +1000

    Add files via upload

---
+++ SMBv5/Code/game_data.py
@@ -0,0 +1,11 @@
+level_1 = {
+        'terrain': 'C:/Pygame/SMBv4/Levels/1/Level_1_terrain.csv',
+        'background': 'C:/Pygame/SMBv4/Levels/1/Level_1_background.csv',
+        'constraints': 'C:/Pygame/SMBv4/Levels/1/Level_1_constraints.csv',
+        'enemies': 'C:/Pygame/SMBv4/Levels/1/Level_1_enemy.csv',
+        'player': 'C:/Pygame/SMBv4/Levels/1/Level_1_player.csv',
+        'blocks': 'C:/Pygame/SMBv4/Levels/1/level_1_blocks.csv',
+        'question_block': 'C:/Pygame/SMBv4/Levels/1/level_1_question_block.csv',
+        'flag': 'C:/Pygame/SMBv4/Levels/1/level_1_flag.csv',
+        'flag_pole': 'C:/Pygame/SMBv4/Levels/1/level_1_flag_pole.csv',
+        'house' :'C:/Pygame/SMBv4/Levels/1/level_1_house.csv'}
